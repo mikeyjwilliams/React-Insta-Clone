c22b14109cc00fd629542e59190babce69a83458
Michael Williams <manicdesigner@gmail.com>
2019-06-12 18:57:40 -0400

    go back to simple for now.

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,27 +1,56 @@
 import React from 'react';
 
 import dummyData from './dummy-data';
+import PostContainer from './components/PostContainer/Postcontainer';
 import './App.css';
 
+const testData = [
+  {
+    id: "a",
+    username: "philzcoffee",
+    thumbnailUrl:
+      "https://tk-assets.lambdaschool.com/ecd33d34-c124-4b75-92d2-e5c52c171ed8_11201517_887808411287357_1307163552_a.jpg",
 
-
-class App extends React.Component {
-  constructor(props) {
-    this.state.tweets = dummyData
+    imageUrl:
+      "https://tk-assets.lambdaschool.com/69cf901b-f96d-466e-a745-ff2a01effac9_philz-image.jpg",
+    likes: 400,
+    timestamp: "July 17th 2017, 12:42:40 pm",
+    comments: [
+      {
+        id: 1,
+        username: "philzcoffee",
+        text:
+          "We've got more than just delicious coffees to offer at our shops!"
+      },
+      {
+        id: 2,
+        username: "biancasaurus",
+        text: "Looks delicious!"
+      },
+      {
+        id: 3,
+        username: "martinseludo",
+        text: "Can't wait to try it!"
+      }
+    ]
   }
+]
 
-render() {
-  console.log(dummyData);
-  return (
-    <div className="ig-layout">
-      {}
-    </div>
-  );
+function App() {
+ return ( 
+    console.log(dummyData);
+    return (
+      <div className="ig-layout">
+        { this.testData.map( (igData) => {
+            <PostContainer
+              key={igData.id}
+              instagramData={igData}
+            />
+          })
+        }
+      </div>
+    );
 }
-
-  
-
-
 export default App;
 
 //1. import dummy data
