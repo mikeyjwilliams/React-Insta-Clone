941612ac0601abc424412fa15f066f61a5b7d4aa
Michael Williams <manicdesigner@gmail.com>
2019-06-12 09:39:23 -0400

    app.js => remove react landing page

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -3,25 +3,11 @@ import React from 'react';
 import dummyData from './dummy-data';
 import './App.css';
 
-
 function App() {
   console.log(dummyData);
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
