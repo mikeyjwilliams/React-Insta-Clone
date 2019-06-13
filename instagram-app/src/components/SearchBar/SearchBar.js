050343caa644b9f64a20eba7ba0e26f28da0d84b
Michael Williams <manicdesigner@gmail.com>
2019-06-12 20:52:48 -0400

    Built out search bar

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,28 +1,39 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram, faCompass } from '@fortawesome/free-brands-svg-icons';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import {  faCompass, faHeart, faUser } from '@fortawesome/free-solid-svg-icons';
 import './SearchBar.css';
 
 const instagramIcon = <FontAwesomeIcon icon={faInstagram} />
-const compassSearch = <FontAwesomeIcon icon={faCompass} />
-
+const compassIcon = <FontAwesomeIcon icon={faCompass} />
+const heartIcon = <FontAwesomeIcon icon={faHeart} />
+const userIcon = <FontAwesomeIcon icon={faUser} />
 
 const SearchBar = props => {
     return (
         <div className="search-bar">
             <div className="header-section">
-                <span className="instagram-icon">{instagramIcon}</span>
-                <h1 className="instagram-name">Instagram</h1>
+                <div className="home-icons">
+                    <h4 className="instagram-icon icon">{instagramIcon}</h4>
+                    <h4 className="home-icon-break icon"> |</h4>
+                    <h1 className="instagram-logo icon">Instagram</h1>
+                </div>
+            </div>
                 <input
                     type="text"
                     placeholder="Search..."
                     className="searchbar"
                 />
-                <div>
-                    <span className="compass-search">
-                        {compassSearch}
-                    </span>
+            <div className="user-icons">
+                <div className="compass-icon icon">
+                    {compassIcon}
+                </div>
+                <div className="heart-icon icon">
+                    {heartIcon}
+                </div>
+                <div className="user-icon icon">
+                    {userIcon}
                 </div>
             </div>         
         </div>
