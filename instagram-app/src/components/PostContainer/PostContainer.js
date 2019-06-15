4fafea83c600c365b5e5e75c0d1dfdaf762ac7d2
Michael Williams <manicdesigner@gmail.com>
2019-06-15 14:18:52 -0400

    feat(PostContainer.js) react component convert

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -7,33 +7,42 @@ import './PostContainer.css';
 
 
 
-const PostContainer = props => {
+class PostContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instagramComments: props.igData.comments
+    }
+  }
+  render() {
+    console.log(this.state.instagramComments);
     return (
       <div className="ig-post">
         <div className="ig-header">
           <img
             className="ig-post-thumbnail"
-            src={props.igData.thumbnailUrl}
-            alt={props.igData.username}
+            src={this.props.igData.thumbnailUrl}
+            alt={this.props.igData.username}
           />
-          <h2 className="ig-post-username">{props.igData.username}</h2>
+          <h2 className="ig-post-username">{this.props.igData.username}</h2>
         </div>
         <div className="ig-photo">
-          <img className="ig-photo" src={props.igData.imageUrl} alt="" />
+          <img className="ig-photo" src={this.props.igData.imageUrl} alt="" />
         </div>
         <div className="ig-interact-section">
-         <LikesSection igData={props.igData}/>
+         <LikesSection igData={this.props.igData}/>
           <div className="ig-comments">
             <CommentContainer 
-                igData={props.igData}
-                addNewComment={props.addNewComment}
-                changeComment={props.changeComment}
-                commentValue={props.commentValue}
+                igData={this.props.igData}
+                addNewComment={this.props.addNewComment}
+                changeComment={this.props.changeComment}
+                commentValue={this.props.commentValue}
             />
           </div>
         </div>
       </div>
     ); 
+  }  
 };
 
 PostContainer.prototypes = {
@@ -46,15 +55,4 @@ PostContainer.prototypes = {
     comments: PropTypes.arrayOf(PropTypes.object)
   })
 };
-
 export default PostContainer;
-
-//1. import dummy data
-//2. iterate over data => pass individual object = {prop} instanceOf {PostContainer}
-//3. {PostContainer} => PASS array comments => {prop} instanceOf {CommentSection}
-//4. {CommentSection} RECIEVE {array} => COMMENTS = {props} && RENDER {CommentComponent} w/ =>
-// { username=>poster, post=>text} (!input box can be added will be incremented tomorrow though not needed now!)
-//5. check TYPES of? DATA passing around as {props} in COMPONENTS
-//6. (not sure yet) Linked to {CommentComponent} =>THAT {CommentSection} will RENDER to-WHERE? {PostComponent} container will RENDER
-//7. FREE TO USE Reactstrap for themeing and styling
-//8. add own styles for theming and styling.
