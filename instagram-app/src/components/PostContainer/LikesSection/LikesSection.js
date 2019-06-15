0c55f08d0de82a51572a5d3650bd47dbd94e2116
Michael Williams <manicdesigner@gmail.com>
2019-06-15 14:12:40 -0400

    feat(LisesSection.js) + function for likes

---
+++ instagram-app/src/components/PostContainer/LikesSection/LikesSection.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './LikesSection.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faComment } from '@fortawesome/free-solid-svg-icons';
+
+const igHeart = <FontAwesomeIcon size="2x" icon={faHeart} />;
+const igComment = <FontAwesomeIcon size="2x" icon={faComment} />;
+
+const LikesSection = props => {
+    return(
+        <div>
+            <div className="ig-interaction">
+                <div className="far ig-heart">{igHeart}</div>
+                <div className="far ig-comment">{igComment}</div>
+            </div>
+            <div className="ig-likes">{props.igData.likes} likes</div>
+        </div>
+        
+    );
+};
+export default LikesSection;
