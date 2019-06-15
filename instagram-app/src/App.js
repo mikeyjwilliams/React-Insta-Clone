5a0d32ca20105d785bf28d1f2f2a79441ea43ea2
Michael Williams <manicdesigner@gmail.com>
2019-06-15 09:32:04 -0400

    docs(App.js) - tooks comments from bottom away

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -15,15 +15,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
-
-//1. import dummy data
-//2. iterate over data => pass individual object = {prop} instanceOf {PostContainer}
-//3. {PostContainer} => PASS array comments => {prop} instanceOf {CommentSection}
-//4. {CommentSection} RECIEVE {array} => COMMENTS = {props} && RENDER {CommentComponent} w/ =>
-// { username=>poster, post=>text} (!input box can be added will be incremented tomorrow though not needed now!)
-//5. check TYPES of? DATA passing around as {props} in COMPONENTS
-//6. (not sure yet) Linked to {CommentComponent} =>THAT {CommentSection} will RENDER to-WHERE? {PostComponent} container will RENDER
-//7. FREE TO USE Reactstrap for themeing and styling
-//8. add own styles for theming and styling.
