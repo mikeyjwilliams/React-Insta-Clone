7ce0d2df2c662791607495b18c6b3e62f324488a
Michael Williams <manicdesigner@gmail.com>
2019-06-16 09:20:27 -0400

    format(PostContainer.js) + remove extra spaces + data name change for consistency + comments in event handler - remove console.logs

--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -5,58 +5,48 @@ import CommentContainer from '../CommentSection/CommentContainer';
 import LikesSection from './LikesSection/LikesSection';
 import './PostContainer.css';
 
-
-
 class PostContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      instagramComments: props.igData.comments,
+      instagramComments: props.instagramData.comments,
       text: ''
     }
   }
-
   
-
   addCommentHandler = e => {
     e.preventDefault();
-    const newComment = {text: this.state.text, username: 'Developer'};
-    const comments = this.state.instagramComments.slice();
-    comments.push(newComment);
+    const newComment = {username: 'Developer', text: this.state.text}; // set name for now.
+    const comments = this.state.instagramComments.slice(); // turning state of comments into an array.
+    comments.push(newComment); // push new comment onto end of comment section.
     this.setState({ instagramComments: comments});
-    this.setState({text: this.state.text});
-    
   }
   
   commentChange = e => {
     this.setState({
       text: e.target.value
     })
-    console.log(e.target.value);
   }
 
-
   render() {
-    console.log(this.state.instagramComments);
     return (
       <div className="ig-post">
         <div className="ig-header">
           <img
             className="ig-post-thumbnail"
-            src={this.props.igData.thumbnailUrl}
-            alt={this.props.igData.username}
+            src={this.props.instagramData.thumbnailUrl}
+            alt={this.props.instagramData.username}
           />
-          <h2 className="ig-post-username">{this.props.igData.username}</h2>
+          <h2 className="ig-post-username">{this.props.instagramData.username}</h2>
         </div>
         <div className="ig-photo">
-          <img className="ig-photo" src={this.props.igData.imageUrl} alt="" />
+          <img className="ig-photo" src={this.props.instagramData.imageUrl} alt="" />
         </div>
         <div className="ig-interact-section">
-         <LikesSection igData={this.props.igData}/>
+         <LikesSection instagramData={this.props.instagramData}/>
           <div className="ig-comments">
             <CommentContainer 
-                comments={this.props.igData.comments}
-
+                instagramComments={this.state.instagramComments}
                 addCommentHandler={this.addCommentHandler}
                 commentChange={this.commentChange}
                 commentValue={this.commentValue}
