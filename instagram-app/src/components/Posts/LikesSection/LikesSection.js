3df427aaebbfe9380d945a958e82bc4ba595c265
Michael Williams <manicdesigner@gmail.com>
2019-06-16 18:59:42 -0400

    mod(LikeSection.js) + icons are appearing correctly

--- instagram-app/src/components/Posts/LikesSection/LikesSection.js
+++ instagram-app/src/components/Posts/LikesSection/LikesSection.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './LikesSection.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faComment } from '@fortawesome/free-solid-svg-icons';
+
+const heartIcon = <FontAwesomeIcon icon={faHeart} size="2x" />
+const commentIcon = <FontAwesomeIcon icon={faComment} size="2x" />
+
 
 const LikesSection = props => {
     return(
         <div>
             <div className="ig-likes-section">
                 
-                <div className="far ig-heart"><FontAwesomeIcon className="far" icon="heart" /></div>
-                <div className="far ig-comment"><FontAwesomeIcon icon="comment" /></div>
+                <div className="far ig-heart">{heartIcon}</div>
+                <div className="far ig-comment">{commentIcon}</div>
             </div>
             <div className="ig-likes">{props.instagramData.likes} likes</div>
         </div>
