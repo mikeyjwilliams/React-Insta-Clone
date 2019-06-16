ab4b8f1afaf47c392dc35b35e908100fa4e7d44b
Michael Williams <manicdesigner@gmail.com>
2019-06-16 17:18:48 -0400

    feat(LikeSection.js) / using fontawesome in different way to showcase emoji icons. -- goal to showcase thin lined ones in future.

--- instagram-app/src/components/PostContainer/LikesSection/LikesSection.js
+++ instagram-app/src/components/PostContainer/LikesSection/LikesSection.js
@@ -2,17 +2,14 @@ import React from 'react';
 import './LikesSection.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart, faComment } from '@fortawesome/free-solid-svg-icons';
-
-const igHeart = <FontAwesomeIcon size="2x" icon={faHeart} />;
-const igComment = <FontAwesomeIcon size="2x" icon={faComment} />;
 
 const LikesSection = props => {
     return(
         <div>
-            <div className="ig-interaction">
-                <div className="far ig-heart">{igHeart}</div>
-                <div className="far ig-comment">{igComment}</div>
+            <div className="ig-likes-section">
+
+                <div className="far ig-heart"><FontAwesomeIcon className="far" icon="heart" /></div>
+                <div className="far ig-comment"><FontAwesomeIcon icon="comment" /></div>
             </div>
             <div className="ig-likes">{props.instagramData.likes} likes</div>
         </div>
