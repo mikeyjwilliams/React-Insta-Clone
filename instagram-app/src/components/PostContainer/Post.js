f816f194699c1ecd193fe1ed93c17d2d85a1a3d5
Michael Williams <manicdesigner@gmail.com>
2019-06-17 09:38:13 -0400

    feat(Post.js) + console.log show datat coming in + div for header section + span for img prop thumbnail

--- instagram-app/src/components/PostContainer/Post.js
+++ instagram-app/src/components/PostContainer/Post.js
@@ -2,7 +2,21 @@ import React from 'react';
 import './Post.css';
 
 const Post = props => {
-
+    console.log('POST DATA', props.instagram);
+    return (
+        <div className="ig-post">
+            <div className="ig-header">
+                <span 
+                    className="ig-icon">
+                        <img 
+                            src={props.instagram.thumbnailUrl} 
+                            alt=""
+                            className="ig-thumbnail"
+                        />
+                </span>
+            </div>
+        </div>
+    );
 }
 
 export default Post;
