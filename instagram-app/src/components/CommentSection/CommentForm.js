82a61dcfba3157087d12aa5984eb72315d9f2d0a
Michael Williams <manicdesigner@gmail.com>
2019-06-17 13:43:36 -0400

    moved form handlers to another component

--- instagram-app/src/components/CommentSection/CommentForm.js
+++ instagram-app/src/components/CommentSection/CommentForm.js
@@ -1,40 +1,19 @@
 import React from 'react';
 import './Comment.css';
 
-class CommentForm extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      text: ''
-    };
-  }
-
-  commentSubmit = e => {
-    e.preventDefault();
-    console.log(this.state.text);
-  };
-
-  textHandler = e => {
-    console.log(e.target.value);
-    this.setState({
-      text: e.target.value
-    });
-  };
-
-  render() {
-    return (
-      <form className="comment-form" onSubmit={this.commentSubmit}>
-        <input
-          type="text"
-          name="new-comment"
-          onChange={this.textHandler}
-          value={this.state.text}
-          placeholder="Add a Comment..."
-          className="ig-comment-input"
-        />
-        <span>elipsis</span>
-      </form>
-    );
-  }
-}
+const CommentForm = props => {
+  return (
+    <form className="comment-form" onSubmit={props.commentSubmit}>
+      <input
+        type="text"
+        name="new-comment"
+        onChange={props.textHandler}
+        value={props.commentValue}
+        placeholder="Add a Comment..."
+        className="ig-comment-input"
+      />
+      <span>elipsis</span>
+    </form>
+  );
+};
 export default CommentForm;
