ef05b2c08484d37e557918e170040bb0328c2e1c
Michael Williams <manicdesigner@gmail.com>
2019-06-17 12:10:39 -0400

    feat(CommentForm.js) + class component + added state w/ text default empty string + event handler of commentSubmit onSubmit + event handler onChange of textHandler + value of input is = this.state.text

--- instagram-app/src/components/CommentSection/CommentForm.js
+++ instagram-app/src/components/CommentSection/CommentForm.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import './Comment.css';
 
-const textHandler = e => {
-  console.log(e.target.value);
-};
+class CommentForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: ''
+    };
+  }
 
-const CommentForm = props => {
-  return (
-    <form className="comment-form">
-      <input
-        type="text"
-        name="new-comment"
-        onChange={textHandler}
-        placeholder="Add a Comment..."
-        className="ig-comment-input"
-      />
-      <span>elipsis</span>
-    </form>
-  );
-};
+  commentSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.text);
+  };
+
+  textHandler = e => {
+    console.log(e.target.value);
+    this.setState({
+      text: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <form className="comment-form" onSubmit={this.commentSubmit}>
+        <input
+          type="text"
+          name="new-comment"
+          onChange={this.textHandler}
+          value={this.state.text}
+          placeholder="Add a Comment..."
+          className="ig-comment-input"
+        />
+        <span>elipsis</span>
+      </form>
+    );
+  }
+}
 export default CommentForm;
