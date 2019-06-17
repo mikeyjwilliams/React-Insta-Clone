06162f67d4a64a2167d5841d902a57c8146e44d1
Michael Williams <manicdesigner@gmail.com>
2019-06-17 07:11:50 -0400

    feat(CommentSection.js) + functional component start + TEST props. pass is working

---
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Comments.css';
+
+const CommentSection = props => {
+    console.log('props.instagramData ', props.instagram);
+    return (
+        <div className="ig-post">
+            
+        </div>
+    );
+}
+export default CommentSection;
