42caa71d7761d6503785c950ddf5d58b68e93ad3
Michael Williams <manicdesigner@gmail.com>
2019-06-17 11:12:08 -0400

    format(Post.js) eslint, prettier format code

--- instagram-app/src/components/PostContainer/Post.js
+++ instagram-app/src/components/PostContainer/Post.js
@@ -1,38 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Comment from '../CommentSection/Comment';
 import './Post.css';
 
 const Post = props => {
-    console.log('POST DATA', props.instagram);
-    return (
-        <div className="ig-post">
-            <div className="ig-header">
-                <span 
-                    className="ig-icon">
-                        <img 
-                            src={props.instagram.thumbnailUrl} 
-                            alt=""
-                            className="ig-thumbnail"
-                        />
-                </span>
-                <span
-                    className="ig-user">
-                        {props.instagram.username}
-                    </span>
-            </div>
-            <div className="ig-image">
-                <img src={props.instagram.imageUrl} alt="" />
-            </div>
-        </div>
-    );
-}
+  console.log('POST DATA', props.instagram);
+  return (
+    <div className="ig-post">
+      <div className="ig-header">
+        <span className="ig-icon">
+          <img
+            src={props.instagram.thumbnailUrl}
+            alt=""
+            className="ig-thumbnail"
+          />
+        </span>
+        <span className="ig-user">{props.instagram.username}</span>
+      </div>
+      <div className="ig-image">
+        <img src={props.instagram.imageUrl} alt="" />
+      </div>
+      {props.instagram.comments.map((comment, i) => {
+        return <Comment key={i} comment={comment} />;
+      })}
+    </div>
+  );
+};
 
 Post.prototypes = {
-    instagram: PropTypes.shape({
-        thumbnailUrl: PropTypes.string,
-        username: PropTypes.string,
-        imageUrl: PropTypes.string
-    })
-}
+  instagram: PropTypes.shape({
+    thumbnailUrl: PropTypes.string,
+    username: PropTypes.string,
+    imageUrl: PropTypes.string
+  })
+};
 
 export default Post;
