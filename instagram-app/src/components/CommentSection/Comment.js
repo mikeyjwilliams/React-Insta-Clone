5a71efe9598e5646f4a9403938e689ee90945e73
Michael Williams <manicdesigner@gmail.com>
2019-06-17 11:11:21 -0400

    feat(Comment.js) + user-comments  section + username displayed + text string comment displayed mappoing these comments into the Post component so all comments will show up underneath the (to be made) icons for liking the insta photo

--- instagram-app/src/components/CommentSection/Comment.js
+++ instagram-app/src/components/CommentSection/Comment.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-import Post from '../PostContainer/Post';
+//import Post from '../PostContainer/Post';
 import './Comment.css';
 
 const Comment = props => {
-    console.log('props.instagramData ', props.instagram);
-    return (
-        <div className="ig-comments">
-            
-        </div>
-    );
-}
+  console.log('props.COMMENTS ', props.comment);
+  return (
+    <div className="ig-comments">
+      {/* <LikeContainer /> */}
+      <div className="user-comments">
+        <span className="user">{props.comment.username}</span>
+        <span className="comment-text">{props.comment.text}</span>
+      </div>
+    </div>
+  );
+};
 export default Comment;
